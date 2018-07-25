40c95a447705c8edbd5342228f89ac4d92d94834
Ramon Sotério Jovencio <ramon.jovencio@thomsonreuters.com>
2018-07-25 12:54:53 -0300

    router added, navbar changed

--- employees-app/src/app/app.module.ts
+++ employees-app/src/app/app.module.ts
@@ -2,23 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { TopComponent } from './top/top.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: '**',        component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
     TopComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ ],
   bootstrap: [AppComponent]
